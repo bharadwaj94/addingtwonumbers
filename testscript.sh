57c0854b49dade7251055b8caf9efd3ba27be571
bharadwaj94 <46172751+bharadwaj94@users.noreply.github.com>
2019-08-12 14:43:28 -0500

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo " this is git hub"
 echo " code is pulled from github"
 echo " $parm is parameter"
-num=$(($firstnumber+$secondnumber))
+num=$(($firstnumber + $secondnumber))
 echo "yeah hook is sucessful now fuck off69"
